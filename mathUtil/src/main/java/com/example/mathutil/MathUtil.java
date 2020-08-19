ce8f6e3181e6188e579c658aa30d8c5ade0928fb
Mezreal <maoyuze@apical.com.cn>
2020-08-19 17:46:58 +0800

    将代码提交到仓库

---
+++ mathUtil/src/main/java/com/example/mathutil/MathUtil.java
@@ -0,0 +1,19 @@
+package com.example.mathutil;
+
+public class MathUtil {
+    public int Add(int param1 , int param2){
+        return param1 + param2;
+    }
+
+    public int Sub(int param1 , int param2){
+        return param1 - param2;
+    }
+
+    public int Mul(int param1 , int param2){
+        return param1 * param2;
+    }
+
+    public int Div(int param1 , int param2){
+        return param1 / param2;
+    }
+}
